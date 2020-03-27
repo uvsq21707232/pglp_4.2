c00eabbc5924e67cafc054cc4d5b4e1d677e88d9
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-03-27 19:08:26 +0100

    tester la classe Quit

---
+++ pglp_4_2/src/test/java/Abdelhafid/pglp_4_2/TestQuit.java
@@ -0,0 +1,21 @@
+package Abdelhafid.pglp_4_2;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class TestQuit {
+	
+	@Test 
+	public void testquit() throws Exception {
+		Stack<Integer> pile = new Stack<Integer>();
+		Undo undo= new Undo(pile);
+		Interpreteur interpret = Interpreteur.initialiser(undo);
+		
+		interpret.executer_cmd("quit");
+	}
+
+
+}
+
+
