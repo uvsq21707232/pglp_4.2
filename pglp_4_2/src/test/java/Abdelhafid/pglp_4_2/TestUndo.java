7172f33e524d180a02cb7fafaa752be856481e00
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-03-27 19:07:39 +0100

    tester Undo

---
+++ pglp_4_2/src/test/java/Abdelhafid/pglp_4_2/TestUndo.java
@@ -0,0 +1,30 @@
+package Abdelhafid.pglp_4_2;
+
+import static org.junit.Assert.*;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
+public class TestUndo {
+	
+	@Test
+	public void test() {
+	Stack<Integer> pile_test = new Stack<Integer>();
+	Undo undo = new Undo(pile_test);
+	
+	pile_test.push(7);
+	undo.addHist();
+	pile_test.push(3);
+	undo.addHist();
+	pile_test.push(4);
+	undo.addHist();
+	undo.apply();
+	
+	Stack<Integer> resultat = new Stack<Integer>();
+	resultat.push(7);
+	resultat.push(3);
+	assertEquals(pile_test, resultat);
+
+}
+}
